168cf5a227d7b7a7eb62243c181f8a06509f7cc0
Mac-ziis <macg286@gmail.com>
2023-07-26 20:38:18 -0700

    Add the second functioning test to potion-manager.test.js

--- __tests__/potion-manager.test.js
+++ __tests__/potion-manager.test.js
@@ -15,4 +15,21 @@ describe('PotionManager', () => {
     expect(PotionManager.allPotions[0].name).toBe("Health Potion");
     expect(PotionManager.allPotions[0].effect).toBe("Restores 50 health points");
   });
+
+  test("should add multiple potions to allPotions", () => {
+    const potionObject1 = { name: "Health Potion", effect: "Restores 50 health points"};
+    const potionObject2 = { name: "Strength Elixir", effect: "Increases attack damage by 10"};
+    PotionManager.addPotion(potionObject1);
+    PotionManager.addPotion(potionObject2);
+
+    expect(PotionManager.allPotions).toHaveLength(2);
+
+    expect(PotionManager.allPotions[0]).toBeInstanceOf(Potion);
+    expect(PotionManager.allPotions[0].name).toBe("Health Potion");
+    expect(PotionManager.allPotions[0].effect).toBe("Restores 50 health points");
+
+    expect(PotionManager.allPotions[1]).toBeInstanceOf(Potion);
+    expect(PotionManager.allPotions[1].name).toBe("Strength Elixir");
+    expect(PotionManager.allPotions[1].effect).toBe("Increases attack damage by 10");
+  });
 });
