f219aa7c6739585582a49e47ffcea22be2fc121b
Mac-ziis <macg286@gmail.com>
2023-07-26 19:46:38 -0700

    Add two more passing tests to potion.test.js for the potion use function

--- __tests__/potion.test.js
+++ __tests__/potion.test.js
@@ -1,4 +1,4 @@
-import Potion from './src/js/potion.js';
+import Potion from '../src/js/potion.js';
 
 describe('newPotion', () => {
   //constructor: return the properties of 'heal' and 'potion'
@@ -12,11 +12,18 @@ describe('newPotion', () => {
 
 describe("Potion", () => {
 
-  test('should restore 50 health pints to the target player', () => {
+  test('should restore 50 health points to the target player', () => {
     const target = {health: 100};
     const potion = new Potion("Health Potion", "Restores 50 health points");
     potion.applyEffect(target);
     expect(target.health).toBe(150);
   });
-})
+
+  test('Should increase the attack power of the target by 10', () => {
+    const target = {attackPower: 50};
+    const potion = new Potion("Strength Elixir", "Increases attack damage by 10");
+    potion.applyEffect(target);
+    expect(target.attackPower).toBe(60);
+  });
+});
 
--- src/js/potion.js
+++ src/js/potion.js
@@ -11,17 +11,17 @@ export default class Potion {
     const mana_elixir_effect = "Restores 25 mana points";
     
     if (this.effect === health_potion_effect) {
-      if (target.hasOwnProperty("health") && typeOf target.health === "number") {
+      if (target.hasOwnProperty("health") && typeof target.health === "number") {
         const healthToRestore = 50;
         target.health += healthToRestore;
       }
     } else if (this.effect === strength_elixir_effect) {
-      if (target.hasOwnProperty("attackPower") && typeOf target.attackPower === "number") {
+      if (target.hasOwnProperty("attackPower") && typeof target.attackPower === "number") {
         const powerIncrease = 10;
         target.attackPower += powerIncrease;
       }
     } else if (this.effect === mana_elixir_effect) {
-      if (target.hasOwnProperty("mana") && typeOf target.mana === "number") {
+      if (target.hasOwnProperty("mana") && typeof target.mana === "number") {
         const manaRestored = 25;
         target.mana += manaRestored;
       }
